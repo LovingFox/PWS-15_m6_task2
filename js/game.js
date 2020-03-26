5ac0dcaa3a551aa7e6592d0cf383a095808e8d2a
LovingFox <LovingFox@GMail.com>
2020-03-26 17:53:38 +0300

    all fixit and todo

--- js/game.js
+++ js/game.js
@@ -2,48 +2,70 @@ const numDivs = 36;
 const maxHits = 10;
 
 let hits = 0;
+let clicks = 0;
 let firstHitTime = 0;
+let divSelector = undefined;
 
 function round() {
-  // FIXME: надо бы убрать "target" прежде чем искать новый
-
-  let divSelector = randomDivId();
-  $(divSelector).addClass("target");
-  // TODO: помечать target текущим номером
-
-  // FIXME: тут надо определять при первом клике firstHitTime
+  if( typeof divSelector != "undefined" ) {
+     $(divSelector).removeClass("target");
+     $(divSelector).text("");
+  }
 
   if (hits === maxHits) {
     endGame();
   }
+  else {
+     divSelector = randomDivId();
+     $(divSelector).addClass("target");
+     $(divSelector).text(hits + 1);
+  }
+}
+
+function initGame() {
+   hits = 0;
+   clicks = 0;
+   firstHitTime = 0;
+   round();
+   $("#win-message").addClass("d-none");
+   $(".game-field").removeClass("d-none");
+   $("#status-message").text("Первый щелчек по квадрату запускает игру");
 }
 
 function endGame() {
-  // FIXME: спрятать игровое поле сначала
-
   let totalPlayedMillis = getTimestamp() - firstHitTime;
   let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
-  $("#total-time-played").text(totalPlayedSeconds);
+  let score = maxHits - clicks + hits;
 
+  $("#status-message").text("Игра завершена");
+  $("#total-time-played").text(totalPlayedSeconds);
+  $("#score").text(score);
+  $(".game-field").addClass("d-none");
   $("#win-message").removeClass("d-none");
 }
 
 function handleClick(event) {
-  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
+  clicks = clicks +1;
+  if( clicks === 1 ) {
+     firstHitTime = getTimestamp();
+     $("#status-message").text("Время пошло...");
+  }
   if ($(event.target).hasClass("target")) {
     hits = hits + 1;
     round();
   }
-  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+  else {
+    $(event.target).addClass("miss");
+    setTimeout(function(){ $(event.target).removeClass("miss"); }, 100);
+  }
 }
 
 function init() {
-  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
-  round();
+  initGame();
 
   $(".game-field").click(handleClick);
   $("#button-reload").click(function() {
-    location.reload();
+    initGame();
   });
 }
 
